d05d75eb4022dc5a73c36383e134156a61c1e881
TiyaRawal <tiyarawal1409@gmail.com>
2026-05-23 15:21:41 +0530

    first commit

--- index.js
+++ index.js
@@ -53,6 +53,8 @@ app.use(cors({
     "http://localhost:3000", "http://localhost:3001",
     "http://localhost:5173", "http://localhost:5174",
     "https://your-frontend.onrender.com",
+    "https://equipment-user.onrender.com",
+    
   ],
   credentials: true,
   methods: ["GET", "POST", "PUT", "DELETE"],
